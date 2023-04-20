d9fa11abd4a36b53d169d39a1d5e1e415d70eed8
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-20 12:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function getFormvalue() {
     //Write your code here
     let str = document.querySelectorAll("input");
 	console.log(str);
-	let s;
+	let s="";
 	for(let i=0;i<2;i++){
-		s=s+str[i].value;
+		s=s+str[i].value+" ";
 	}
 	alert(s);
 }
